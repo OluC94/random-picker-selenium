cce7044f88df51ecab1b704393f33cc9b1a99807
OluC94 <coker.olu94@gmail.com>
2024-07-03 16:36:58 +0100

    create tests for verifying initial state

--- src/test/java/com/picker/test/RandomPickerTest.java
+++ src/test/java/com/picker/test/RandomPickerTest.java
@@ -1,4 +1,27 @@
 package com.picker.test;
 
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.*;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
 public class RandomPickerTest {
+
+    @BeforeEach
+    public void setUp() {
+        Configuration.browserSize = "1200x800";
+        open("https://random-picker-olu.netlify.app/");
+    }
+
+    @Test
+    public void randomPickerTest() {
+
+        // Verify initial state
+        $("[data-testid='selected-word']").shouldHave(exactText("Click the button"));
+        $("[data-testid='input-area']").shouldHave(exactText("JavaScript\nPython\nPHP\nJava\nC#"));
+    }
 }
